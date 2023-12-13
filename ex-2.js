1534adf87f2afced8d2d3f8db8603442e592b5d4
Tanawut Wongboot <tanawutwongboot@Mac-mini-khxng-Tanawut.local>
2023-12-13 21:30:19 +0700

    feat(ex-2): inheritance class

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,29 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content, receiver);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content, receiver);
+  }
+}
+
+const myEmail = new EmailNotification(1, 9.0, "Email", "Tang");
+myEmail.send();
+
+const mySMS = new SMSNotification(1, 9.0, "SMS", "0931101931");
+mySMS.send();
